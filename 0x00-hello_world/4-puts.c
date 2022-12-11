670256b48ad2f9a6c2dc0505b19e80ffd41358c9
eeshasal97 <aisha.salau@hotmail.com>
2022-12-11 12:36:57 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+
+ * Return: 0 if executed successfully
+
+ */
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
+}
