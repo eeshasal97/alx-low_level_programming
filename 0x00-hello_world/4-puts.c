6e03223ddc619a96fc43117f7f484d92651638af
eeshasal97 <aisha.salau@hotmail.com>
2022-12-11 13:48:39 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
 /**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-* main - prints the phrase "Programming is like building a multilingual puzzle
-
-* Return: 0 if executed successfully
-*/
+#include <stdio.h>
 
 int main(void)
-  
 {
+  puts("\"Programming is like building a multilingual puzzle");
 
-puts("\"Programming is like building a multilingual puzzle");
-  
-return (0);
+  return (0);
 }
